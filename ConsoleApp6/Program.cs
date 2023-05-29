d6fb4bc33389a92a133aaa6bcf7b9d5d81303ad0
opilane <opilane@FUJI-YMHP016311>
2023-05-29 12:58:05 +0300

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LINQ");
+
+            SumLINQ();
+            MaxLINQ();
+        }
+        
+        //Sum() funktsioon liidab kõik numbrid listist kokku
+        public static void SumLINQ()
+        {
+            Console.WriteLine("Summa on:");
+            List<int> numbers = new List<int> { 1, 4, 5, 8, 2, 3, 6 };
+            int sum = numbers.Sum();
+            Console.WriteLine(sum);
+        }
+
+        //Max() funktsioon leiab listist suurima numbri
+        public static void MaxLINQ()
+        {
+            Console.WriteLine("Suurim arv listist on:");
+            List<int> numbers = new List<int> { 2, 11, 5, 7, 9, 1, 3 };
+            int max = numbers.Max();
+            Console.WriteLine(max);
+        }
+    }
+}
